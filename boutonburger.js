c62e19fc90c28b712563b2fdf82263a909309ab0
Eragonns <lecomte.franck@live.fr>
2024-05-13 14:44:43 +0200

    bouton burger

--- boutonburger.js
+++ boutonburger.js
@@ -1,13 +1,9 @@
-var navbar__burger = document.getElementById("lanavbar__burger");
-var ouverture = document.getElementById("ouverture");
-var fermeture = document.getElementById("fermeBtn");
-
-ouverture.onclick = openNav;
-fermeture.onclick = closeNav;
-
-function openNav() {
-    navbar__burger.classList.add("active");
-}
-function closeNav() {
-    navbar__burger.classList.remove("active");
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBurger = document.querySelector(".menu-burger");
+    const navLinks = document.querySelector(".nav-links");
+  
+    menuBurger.addEventListener('click', function() {
+        navLinks.classList.toggle('menu-mobile');
+    });
+  });
+  
